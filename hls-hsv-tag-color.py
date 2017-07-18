17226e9a72721b0374b129521126499354305a06
Gaute Hope <eg@gaute.vetsj.com>
2017-07-18 11:54:53 +0200

    return tag colors for Utils::get_tags_colors

--- hls-hsv-tag-color.py
+++ hls-hsv-tag-color.py
@@ -50,41 +50,46 @@ class HlsHsvTagColorPlugin (GObject.Object):
   def do_deactivate (self):
     print ('hls-hsv-tag-color: deactivate')
 
+  def color_tag (self, bg, tag, selected):
+    m = md5 (tag.encode('ascii', 'replace')).hexdigest()[-6:]
+    m = int (m, 16)
+
+    hue = m / 0xffffff * 1.0
+
+    if self.colorspace == 'hls':
+      (r, g, b) = colorsys.hls_to_rgb (hue, self.saturation, self.lightness)
+    else:
+      (r, g, b) = colorsys.hsv_to_rgb (hue, self.saturation, self.value)
+
+    r = int(r * 255)
+    g = int(g * 255)
+    b = int(b * 255)
+    a = int(self.alpha * 255)
+
+    tc = (r, g, b, a)
+
+    # parse bg color
+    (rb, gb, bb) = struct.unpack ('BBB', bytes.fromhex(bg[1:7]))
+
+    # luminocity
+    lum = ((self.alpha * r + (1-self.alpha) * rb) * .2126 +
+           (self.alpha * g + (1-self.alpha) * gb) * .7152 +
+           (self.alpha * b + (1-self.alpha) * bb)  * .0722) / 255.0;
+
+    if lum > 0.5:
+      fc = (0, 0, 0, 0)
+    else:
+      fc = (255, 255, 255, 255)
+
+    return (fc, tc)
+
+
   def do_format_tags (self, bg, tags, selected):
     newtags = []
 
     # vary hue based on tag md5: saturation and value remain constant.
     for t in tags:
-      m = md5 (t.encode('ascii', 'replace')).hexdigest()[-6:]
-      m = int (m, 16)
-
-      hue = m / 0xffffff * 1.0
-
-      if self.colorspace == 'hls':
-        (r, g, b) = colorsys.hls_to_rgb (hue, self.saturation, self.lightness)
-      else:
-        (r, g, b) = colorsys.hsv_to_rgb (hue, self.saturation, self.value)
-
-      r = int(r * 255)
-      g = int(g * 255)
-      b = int(b * 255)
-      a = int(self.alpha * 255)
-
-      tc = (r, g, b, a)
-
-      # parse bg color
-      (rb, gb, bb) = struct.unpack ('BBB', bytes.fromhex(bg[1:7]))
-
-      # luminocity
-      lum = ((self.alpha * r + (1-self.alpha) * rb) * .2126 +
-             (self.alpha * g + (1-self.alpha) * gb) * .7152 +
-             (self.alpha * b + (1-self.alpha) * bb)  * .0722) / 255.0;
-
-      if lum > 0.5:
-        fc = "#000000"
-      else:
-        fc = "#ffffff"
-
+      (fc, tc) = self.color_tag (bg, t, selected)
       newtags.append ((tc, fc, t))
 
     return newtags
@@ -101,7 +106,7 @@ class HlsHsvTagColorIndexPlugin (HlsHsvTagColorPlugin, Astroid.ThreadIndexActiva
   def do_format_tags (self, bg, tags, selected):
     tags = HlsHsvTagColorPlugin.do_format_tags (self, bg, tags, selected)
 
-    return "<span size=\"xx-small\"> </span>".join(["<span bgcolor=\"%s\" color=\"%s\"> %s </span>" % (("#%02x%02x%02x%02x" % tc), fc, t) for tc, fc, t in tags])
+    return "<span size=\"xx-small\"> </span>".join(["<span bgcolor=\"%s\" color=\"%s\"> %s </span>" % (("#%02x%02x%02x%02x" % tc), ("#%02x%02x%02x%02x" % fc), t) for tc, fc, t in tags])
 
 
 class HlsHsvTagColorViewPlugin (HlsHsvTagColorPlugin, Astroid.ThreadViewActivatable):
@@ -123,7 +128,28 @@ class HlsHsvTagColorViewPlugin (HlsHsvTagColorPlugin, Astroid.ThreadViewActivata
         tc, fc, ts = t
         r, g, b, a = tc
         a = a / 255
-        yield "<span style=\"background-color: rgba(%d, %d, %d, %.2f); color: %s !important; white-space: pre;\" > %s </span>" % (r, g, b, a, fc, ts)
+
+        fr, fg, fb, fa = fc
+        fa = fa / 255
+
+        yield "<span style=\"background-color: rgba(%d, %d, %d, %.2f); color: rgba(%d, %d, %d, %.2f) !important; white-space: pre;\" > %s </span>" % (r, g, b, a, fr, fg, fb, fa, ts)
 
     return " ".join(f(tags))
+
+class HlsHsvTagColorAstroidPlugin (HlsHsvTagColorPlugin, Astroid.Activatable):
+  def do_activate (self):
+    HlsHsvTagColorPlugin.do_activate (self)
+
+  def do_deactivate (self):
+    HlsHsvTagColorPlugin.do_deactivate (self)
+
+  def do_get_tag_colors (self, tag, bg):
+    fc, tc = self.color_tag (bg, tag, False)
+
+    _fc = ('rgba(%d,%d,%d,%.2f)' % (*fc[:-1], fc[-1] / 255))
+    _tc = ('rgba(%d,%d,%d,%.2f)' % (*tc[:-1], tc[-1] / 255))
+
+    return [_fc, _tc]
+
+
 
